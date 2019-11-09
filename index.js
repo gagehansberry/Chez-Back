a4aacf9c061d4e68d2f2c2e1ea836c0dee5e6f05
MystroMckky <tfroog@gmail.com>
2019-11-09 15:24:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -226,8 +226,8 @@ app.get('/deletepatient', (req, res) =>{
 //ECOUNTER INFO HTTP CALLS
 //add encounter 
 app.get('/addencounter', (req,res) =>{
-  let patient= {name:'Evan Kosmos', phone_number: 123, address: null, insurance: null, dob: null, gender: null, care_taker:null, medications: null, appointments: null};
-  //let patient = {name:req.body.name, phone_number:req.body.phone_number, address:req.body.address, insurance:req.body.insurance, dob:new Date(req.body.dob), gender:req.body.gender, care_taker:req.body.care_taker, medications:req.body.medications, appointments:req.body.appointments};
+  //let patient= {name:'Evan Kosmos', phone_number: 123, address: null, insurance: null, dob: null, gender: null, care_taker:null, medications: null, appointments: null};
+  let patient = {name:req.body.name, phone_number:req.body.phone_number, address:req.body.address, insurance:req.body.insurance, dob:new Date(req.body.dob), gender:req.body.gender, care_taker:req.body.care_taker, medications:req.body.medications, appointments:req.body.appointments};
   let sql = 'INSERT INTO HealthTrack.encounter_info SET ?';
   let query= db.query(sql, patient, (err, result) => {
     if(err){
